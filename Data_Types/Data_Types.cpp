267c5a3a95fa5fe875960c6bbbcfb6a1b4771e4b
Denis <46909842+username10form@users.noreply.github.com>
2019-03-02 22:23:25 +0200

    Create Data_Types.cpp

---
+++ Data_Types/Data_Types.cpp
@@ -0,0 +1,27 @@
+#include "pch.h"
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main()
+{
+	//собственно , вывод
+	cout << "Bool - " << sizeof(bool) << endl;
+	cout << "Char - " << sizeof(char) << endl;
+	cout << "wchar_t - " << sizeof(wchar_t) << endl;
+	cout << "int - " << sizeof(int) << endl;
+	cout << "float - " << sizeof(float) << endl;
+	cout << "double - " << sizeof(double) << endl;
+	
+
+
+//sizeof - возвращающает длину в байтах переменной или типа в скобках
+
+	//задержка
+	cin.get();
+	return 0;
+
+}
